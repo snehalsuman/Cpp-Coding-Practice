7f37ce7dc512e214c84bfbcf880006395f6800af
Snehal Suman <118334030+snehalsuman@users.noreply.github.com>
2024-09-19 23:05:59 +0530

    Add files via upload

---
+++ 2PalindromeNumber.cpp
@@ -0,0 +1,26 @@
+// Time Complexity= Average(log10(n))
+// Space Complexity=Average(1)
+
+// #include <iostream>
+// using namespace std;
+// bool isPalindrome(int n)
+// {
+//     int x=n, revNum = 0, r;
+//     while (n > 0)
+//     {
+//         r = n % 10;
+//         n = n / 10;
+//         revNum = revNum * 10 + r;
+//     }
+//  return x==revNum;
+// }
+
+// int main()
+// {
+//     int n = 12321;
+//     cout << isPalindrome(n) << endl;
+//     return 0;
+
+//     return 0;
+// }
+
